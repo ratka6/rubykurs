d3438ce826f04c9053c999a9576f60c3a61e4bfc
ratka6 <ratka6@gmail.com>
2015-05-21 17:57:39 +0000

    dodanie linkow

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
-    def show
+    def index
+        @articles = Article.all
         
+    end
+    def show
+        @article =  Article.find(params[:id])
     end
     def new
         
