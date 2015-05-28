873121d58c94b15c552bf44480cf784e7db686ab
ratka6 <ratka6@gmail.com>
2015-05-28 16:40:48 +0000

    dodanie edycji i walidacji

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,17 +3,35 @@ class ArticlesController < ApplicationController
         @articles = Article.all
         
     end
+    
     def show
-        @article =  Article.find(params[:id])
+        @article = Article.find(params[:id])
     end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+        redirect_to @article
+      else
+        render 'edit'
+      end
+    end
+    
     def new
         
     end
+    
     def create
         @article = Article.new(article_params)
-        @article.save
-        
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
     
     private
